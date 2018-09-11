8117c48668de7917adc4a043410bb6fb44d677b3
CondorNegro <lopez.gaston.1996@gmail.com>
2018-09-11 18:55:48 -0300

    Comienzo top

--- TP2/Src/Sources/top_arquitectura.v
+++ TP2/Src/Sources/top_arquitectura.v
@@ -10,53 +10,68 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-`define BUS_DATOS           4   // Tamanio del bus de entrada. (Por ejemplo, cantidad de switches).
-`define BUS_SALIDA          4   // Tamanio del bus de salida. (Por ejemplo, cantidad de leds).
-`define CANT_BOTONES_ALU    4   // Cantidad de botones.
-`define CANT_BIT_OPCODE    4    // Numero de bits del codigo de operacion de la ALU.
+`define BUS_DATOS_ALU       8     // Tamanio del bus de entrada. (Por ejemplo, cantidad de switches).
+`define BUS_SALIDA_ALU      8     // Tamanio del bus de salida. (Por ejemplo, cantidad de leds).
+`define CANT_BIT_OPCODE     8     // Numero de bits del codigo de operacion de la ALU.
+`define WIDTH_WORD          8     // Tamanio de palabra
+`define CANT_BIT_STOP       2     // Cantidad de bit de parada
 
 module top_arquitectura(
   i_clock, 
-  i_reset, 
-  i_switches,
-  i_botones,
-  o_leds 
+  i_reset,
+  i_rx_uart,
+  o_tx_uart 
+  //o_leds 
   );
 
 // Parametros
-parameter BUS_DATOS = `BUS_DATOS;
-parameter BUS_SALIDA = `BUS_SALIDA;
-parameter CANT_BOTONES_ALU = `CANT_BOTONES_ALU;
+parameter BUS_DATOS_ALU = `BUS_DATOS_ALU;
+parameter BUS_SALIDA_ALU = `BUS_SALIDA_ALU;
 parameter CANT_BIT_OPCODE = `CANT_BIT_OPCODE;
+parameter WIDTH_WORD = `WIDTH_WORD;
+parameter CANT_BIT_STOP = `CANT_BIT_STOP;
+
 
 // Entradas - Salidas
 input i_clock;                                  // Clock.
 input i_reset;                                  // Reset.
-input [BUS_DATOS - 1 : 0] i_switches;           // Switches.
-input [CANT_BOTONES_ALU - 1 : 0] i_botones;     // Botones.
-output [BUS_SALIDA - 1 : 0] o_leds;             // Leds.
+input i_rx_uart;          
+output o_tx_uart;
+//output [BUS_SALIDA_ALU - 1 : 0] o_leds;             // Leds.
 
 
 
 // Wires.
-wire [BUS_DATOS - 1 : 0] wire_operando_1;
-wire [BUS_DATOS - 1 : 0] wire_operando_2;
+wire [BUS_DATOS_ALU - 1 : 0] wire_operando_1;
+wire [BUS_DATOS_ALU - 1 : 0] wire_operando_2;
 wire [CANT_BIT_OPCODE - 1 : 0] wire_opcode;
+wire [BUS_SALIDA_ALU - 1 : 0] wire_resultado_alu;
+wire [WIDTH_WORD - 1 : 0] wire_data_rx;
+wire [WIDTH_WORD - 1 : 0] wire_data_tx;
+wire wire_tx_done;
+wire wire_rx_done;
+wire wire_tx_start;
 
 
 // Modulo Configurador. (Logica de seteo de operandos y del codigo de operacion en los registros).
 
 interface_circuit
     #(
-         .CANT_DATOS_ENTRADA (BUS_DATOS),
-         .CANT_BITS_OPCODE_ALU (CANT_BIT_OPCODE)
+         .CANT_DATOS_ENTRADA_ALU (BUS_DATOS_ALU),
+         .CANT_BITS_OPCODE_ALU (CANT_BIT_OPCODE),
+         .CANT_DATOS_SALIDA_ALU (BUS_SALIDA_ALU),
+         .WIDTH_WORD (WIDTH_WORD)
      ) 
    u_interface_circuit1    // Una sola instancia de este modulo
    (
    .i_clock (i_clock),
    .i_reset (i_reset),
-   .i_switches (i_switches),
-   .i_botones (i_botones),
+   .i_resultado_alu (wire_resultado_alu),
+   .i_data_rx (wire_data_rx),
+   .i_tx_done (wire_tx_done),
+   .i_rx_done (wire_tx_done),
+   .o_tx_start (wire_tx_start),
+   .o_data_tx (wire_data_tx),
    .o_reg_dato_A (wire_operando_1),
    .o_reg_dato_B (wire_operando_2),
    .o_reg_opcode (wire_opcode)
@@ -67,8 +82,8 @@ interface_circuit
 
 alu
     #(
-         .CANT_BUS_ENTRADA (BUS_DATOS),
-         .CANT_BUS_SALIDA (BUS_SALIDA),
+         .CANT_BUS_ENTRADA (BUS_DATOS_ALU),
+         .CANT_BUS_SALIDA_ALU (BUS_SALIDA_ALU),
          .CANT_BITS_OPCODE (CANT_BIT_OPCODE)
      ) 
    u_alu1    // Una sola instancia de este modulo
@@ -76,7 +91,7 @@ alu
    .i_operando_1 (wire_operando_1),
    .i_operando_2 (wire_operando_2),
    .i_opcode (wire_opcode),
-   .o_resultado (o_leds)
+   .o_resultado (wire_resultado_alu)
    );
 
 
