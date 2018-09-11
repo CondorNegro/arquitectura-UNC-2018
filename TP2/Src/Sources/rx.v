ebf087469c2dbf00d58ee1733531da7243bfc32c
matias <kleiner.matias@gmail.com>
2018-09-11 12:03:41 -0300

    agrego cambios

--- TP2/Src/Sources/rx.v
+++ TP2/Src/Sources/rx.v
@@ -57,16 +57,19 @@ always@( posedge i_rate ) begin //Memory
     else begin
         reg_state <= reg_next_state;
         reg_contador_ticks <= reg_contador_ticks + 1;
+        
         if ((reg_state == READ) && ((reg_contador_ticks % 16) == 0)) begin
             reg_contador_bits <= reg_contador_bits + 1;
+            reg_contador_bits_stop <= 0;
         end
-        else begin 
+
+        else if ((reg_state == STOP) && ((reg_contador_ticks % 16) == 0)) begin
+            reg_contador_bits_stop <= reg_contador_bits_stop + 1;
             reg_contador_bits <= 0;
         end
-        if ((reg_state == STOP) && ((reg_contador_ticks % 16) == 0)) begin
-            reg_contador_bits_stop <= reg_contador_bits_stop + 1;
-        end
+
         else begin 
+            reg_contador_bits <= 0;
             reg_contador_bits_stop <= 0;
         end
         
