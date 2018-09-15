c2b89353f3f449175ae461decb6eee5a10611ae3
CondorNegro <lopez.gaston.1996@gmail.com>
2018-09-15 20:09:19 -0300

    test bench alu

--- TP2/Src/Test_Benches/test_bench_alu.v
+++ TP2/Src/Test_Benches/test_bench_alu.v
@@ -9,9 +9,9 @@
 // Anio 2018.
 //////////////////////////////////////////////////////////////////////////////////
 
-`define BUS_DATOS               4   // Tamanio del bus de entrada. 
-`define BUS_SALIDA              4   // Tamanio del bus de salida. 
-`define CANT_BITS_OPCODE_ALU    4   // Cantidad de bits del codigo de operacion de la ALU.
+`define BUS_DATOS               8   // Tamanio del bus de entrada. 
+`define BUS_SALIDA              8   // Tamanio del bus de salida. 
+`define CANT_BITS_OPCODE_ALU    8   // Cantidad de bits del codigo de operacion de la ALU.
 
 
 module TestBenchAlu();
@@ -32,59 +32,59 @@ module TestBenchAlu();
 	
 	
 	initial	begin
-		#10 reg_operando_1 = 4'b00000000; 
-		#10 reg_opcode = 4'b00000000;
-		#10 reg_operando_2 = 4'b00000000;
+		#10 reg_operando_1 = 8'b00000000; 
+		#10 reg_opcode = 8'b00000000;
+		#10 reg_operando_2 = 8'b00000000;
 		
-		// Test 4: ADD.
-		#50 reg_operando_1 = 4'b00001101;
-        #10 reg_opcode = 4'b00100000; //ADD
-        #10 reg_operando_2 = 4'b00000101;
+		// Test 1: ADD.
+		#50 reg_operando_1 = 8'b00001101;
+        #10 reg_opcode = 8'b00100000; //ADD
+        #10 reg_operando_2 = 8'b00000101;
        
-		// Test 5: SUB.
-		#50 reg_operando_1 = 4'b00000101;
-        #10 reg_opcode = 4'b00100010; //SUB
-        #10 reg_operando_2 = 4'b00000001;
+		// Test 2: SUB.
+		#50 reg_operando_1 = 8'b00000101;
+        #10 reg_opcode = 8'b00100010; //SUB
+        #10 reg_operando_2 = 8'b00000001;
        				
-		// Test 6: AND.
-        #50 reg_operando_1 = 4'b00001101;
-        #10 reg_opcode = 4'b00100100; //AND
-        #10 reg_operando_2 = 4'b00000101;
+		// Test 3: AND.
+        #50 reg_operando_1 = 8'b00001101;
+        #10 reg_opcode = 8'b00100100; //AND
+        #10 reg_operando_2 = 8'b00000101;
                
-        // Test 7: OR.
-         #50 reg_operando_1 = 4'b00000101;
-         #10 reg_opcode = 4'b00100101; //OR
-         #10 reg_operando_2 = 4'b00001101;
+        // Test 4: OR.
+         #50 reg_operando_1 = 8'b00000101;
+         #10 reg_opcode = 8'b00100101; //OR
+         #10 reg_operando_2 = 8'b00001101;
               
 		
-        // Test 8: XOR.
-         #50 reg_operando_1 = 4'b00001101;
-         #10 reg_opcode = 4'b00100110; //XOR
-         #10 reg_operando_2 = 4'b00000101;
+        // Test 5: XOR.
+         #50 reg_operando_1 = 8'b00001101;
+         #10 reg_opcode = 8'b00100110; //XOR
+         #10 reg_operando_2 = 8'b00000101;
               
          
-        // Test 9: SRA.
-		 #50 reg_operando_1 = 4'b00000101;
-         #10 reg_opcode = 4'b00000011; //SRA
-         #10 reg_operando_2 = 4'b00000011;
+        // Test 6: SRA.
+		 #50 reg_operando_1 = 8'b00000101;
+         #10 reg_opcode = 8'b00000011; //SRA
+         #10 reg_operando_2 = 8'b00000011;
            
 		
-		// Test 10: SRL.
-		 #50 reg_operando_1 = 4'b00001101;
-         #10 reg_opcode = 4'b00000010; //SRL
-         #10 reg_operando_2 = 4'b00000011;
+		// Test 7: SRL.
+		 #50 reg_operando_1 = 8'b00001101;
+         #10 reg_opcode = 8'b00000010; //SRL
+         #10 reg_operando_2 = 8'b00000011;
               
 		
-		// Test 11: NOR.
-		 #50 reg_operando_1 = 4'b00000101;
-         #10 reg_opcode = 4'b00100111; //NOR
-         #10 reg_operando_2 = 4'b00000101;
+		// Test 8: NOR.
+		 #50 reg_operando_1 = 8'b00000101;
+         #10 reg_opcode = 8'b00100111; //NOR
+         #10 reg_operando_2 = 8'b00000101;
           
 		
-		// Test 12: Operacion falsa.
-         #50 reg_operando_1 = 4'b00001101;
-         #10 reg_opcode = 4'b00000000; //Ninguna operacion.
-         #10 reg_operando_2 = 4'b00001001;
+		// Test 9: Operacion falsa.
+         #50 reg_operando_1 = 8'b00001101;
+         #10 reg_opcode = 8'b00000000; //Ninguna operacion.
+         #10 reg_operando_2 = 8'b00001001;
 		
 		
 		#1000 $finish;
