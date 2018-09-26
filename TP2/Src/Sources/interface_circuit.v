f21bae80366c7a3b158f2cf9ce6ab7e26f10f149
matias <kleiner.matias@gmail.com>
2018-09-26 14:03:00 -0300

    anda

--- TP2/Src/Sources/interface_circuit.v
+++ TP2/Src/Sources/interface_circuit.v
@@ -19,18 +19,19 @@
 
 
 module interface_circuit( 
-    i_reset,
-    i_resultado_alu,
-    i_data_rx,
-    i_rx_done,
-    i_tx_done,
-    i_clock,
-    o_tx_start,
-    o_data_tx,
-    o_reg_dato_A,
-    o_reg_dato_B,
-    o_reg_opcode
-    );
+   i_reset,
+   i_resultado_alu,
+   i_data_rx,
+   i_rx_done,
+   i_tx_done,
+   i_clock,
+   o_tx_start,
+   o_data_tx,
+   o_reg_dato_A,
+   o_reg_dato_B,
+   o_reg_opcode,
+   o_prueba
+   );
 
 // Parametros.
 parameter CANT_DATOS_ENTRADA_ALU    = `CANT_DATOS_ENTRADA_ALU;
@@ -57,7 +58,7 @@ output reg [CANT_DATOS_ENTRADA_ALU - 1 : 0] o_reg_dato_A;
 output reg [CANT_DATOS_ENTRADA_ALU - 1 : 0] o_reg_dato_B; 
 output reg [CANT_BITS_OPCODE_ALU - 1 : 0] o_reg_opcode;  // Codigo de operacion.      
 
-
+output reg o_prueba;
 
 // Registros.
 reg [ 3 : 0 ] reg_state;
@@ -65,137 +66,136 @@ reg [ 3 : 0 ] reg_next_state;
 reg registro_tx_done;
 reg registro_rx_done;
 
+reg [CANT_DATOS_ENTRADA_ALU - 1 : 0] o_reg_dato_A_next;             
+reg [CANT_DATOS_ENTRADA_ALU - 1 : 0] o_reg_dato_B_next; 
+reg [CANT_BITS_OPCODE_ALU - 1 : 0] o_reg_opcode_next;  // Codigo de operacion.  
+reg [WIDTH_WORD_INTERFACE - 1 : 0]  o_data_tx_next;   
+
 
 always@( posedge i_clock ) begin //Memory
-     // Se resetean los registros.
-    if (~ i_reset) begin
-        reg_state <= 1;
-        registro_tx_done <= 0;
-        registro_rx_done <= 0;
-    end 
+    // Se resetean los registros.
+   if (~ i_reset) begin
+       reg_state <= 1;
+       registro_tx_done <= 0;
+       registro_rx_done <= 0;
+       o_reg_dato_A <= 0;
+       o_reg_dato_B <= 0;
+       o_reg_opcode <= 0;
+       o_data_tx <= 0;
+   end 
 
-    else begin
-        registro_tx_done <= i_tx_done;
-        registro_rx_done <= i_rx_done;
-        reg_state <= reg_next_state;
+   else begin
+       registro_tx_done <= i_tx_done;
+       registro_rx_done <= i_rx_done;
+       reg_state <= reg_next_state;
 
-
-  
-    end
+       o_reg_dato_A <= o_reg_dato_A_next;
+       o_reg_dato_B <= o_reg_dato_B_next;
+       o_reg_opcode <= o_reg_opcode_next;
+       o_data_tx <= o_data_tx_next;
+   end
 end
 
 
 
 
-always@( reg_state, i_rx_done, registro_tx_done ,registro_rx_done , i_tx_done, i_data_rx, i_resultado_alu, i_reset, reg_next_state ) begin //NEXT - STATE logic
-    
-    case (reg_state)
-        
-        ESPERA : begin
-            if ((i_rx_done == 1) && (registro_rx_done == 0)) begin  // Deteccion de flanco ascendente
-                reg_next_state = OPERANDO1;
-            end
-            else begin
-                reg_next_state = ESPERA;
-            end  
-        end
-        
-        OPERANDO1 : begin
-            if ((i_rx_done == 1) && (registro_rx_done == 0)) begin  // Deteccion de flanco ascendente
-                reg_next_state = OPERACION;
-            end
-            else begin
-                reg_next_state = OPERANDO1;
-            end  
-        end
-        
-        OPERACION : begin
+always@( * ) begin //NEXT - STATE logic
+   
+   case (reg_state)
+       
+       ESPERA : begin
            if ((i_rx_done == 1) && (registro_rx_done == 0)) begin  // Deteccion de flanco ascendente
-                reg_next_state = OPERANDO2;
-            end
-            else begin
-                reg_next_state = OPERACION;
-            end  
-        end
-        
-        OPERANDO2 : begin
-            // Deteccion de flanco ascendente
-            // Si se detecta que el registro_tx_done pasa de 0 a 1, se va al estado ESPERA.
-            if ( (i_tx_done == 1) && (registro_tx_done == 0) ) begin
-                reg_next_state = ESPERA;
-            end
-            else begin
-                reg_next_state = OPERANDO2;
-            end        
-        end
-        
-        default begin
-            reg_next_state = ESPERA;
-        end
-    endcase 
+               reg_next_state = OPERANDO1;
+           end
+           else begin
+               reg_next_state = ESPERA;
+           end  
+       end
+       
+       OPERANDO1 : begin
+           if ((i_rx_done == 1) && (registro_rx_done == 0)) begin  // Deteccion de flanco ascendente
+               reg_next_state = OPERACION;
+           end
+           else begin
+               reg_next_state = OPERANDO1;
+           end  
+       end
+       
+       OPERACION : begin
+          if ((i_rx_done == 1) && (registro_rx_done == 0)) begin  // Deteccion de flanco ascendente
+               reg_next_state = OPERANDO2;
+           end
+           else begin
+               reg_next_state = OPERACION;
+           end  
+       end
+       
+       OPERANDO2 : begin
+           // Deteccion de flanco ascendente
+           // Si se detecta que el registro_tx_done pasa de 0 a 1, se va al estado ESPERA.
+           if ( (i_tx_done == 1) && (registro_tx_done == 0)) begin
+               reg_next_state = ESPERA;
+           end
+           else begin
+               reg_next_state = OPERANDO2;
+           end        
+       end
+       
+       default begin
+           reg_next_state = ESPERA;
+       end
+   endcase 
 end
 
 
-always@( i_rx_done, i_tx_done, i_data_rx, i_resultado_alu, i_reset, reg_next_state, reg_state ) begin //Output logic
-    
-    case (reg_state)
-        
-        ESPERA : begin
-            o_tx_start = 0;
-            o_data_tx = o_data_tx;
-            o_reg_dato_A = o_reg_dato_A;
-            o_reg_dato_B = o_reg_dato_B;
-            o_reg_opcode = o_reg_opcode;
-        end
-        
-        OPERANDO1 : begin
-            o_tx_start = 0;
-            o_data_tx = o_data_tx;
-            // Hay una diferencia de tiempo entre la actualizacion del reg_next_state y la del reg_state, por lo que 
-            // puede introducir errores en la carga de los operandos y de los opcodes.
-            if (reg_next_state == OPERACION) begin  
-                o_reg_dato_A = o_reg_dato_A;
-            end
-            else begin
-                o_reg_dato_A = i_data_rx;
-            end
-            o_reg_dato_B = o_reg_dato_B;
-            o_reg_opcode = o_reg_opcode;
-        end
-        
-        OPERACION : begin
-            o_tx_start = 0;
-            o_data_tx = o_data_tx;
-            o_reg_dato_A = o_reg_dato_A;
-            o_reg_dato_B = o_reg_dato_B;
-            // Hay una diferencia de tiempo entre la actualizacion del reg_next_state y la del reg_state, por lo que 
-            // puede introducir errores en la carga de los operandos y de los opcodes.
-            if (reg_next_state == OPERANDO2) begin
-                o_reg_opcode = o_reg_opcode;
-            end
-            else begin
-                o_reg_opcode = i_data_rx;
-            end
-           
-        end
-        
-        OPERANDO2 : begin
-            o_tx_start = 1;
-            o_data_tx = i_resultado_alu; 
-            o_reg_dato_A = o_reg_dato_A;
-            o_reg_dato_B = i_data_rx;
-            o_reg_opcode = o_reg_opcode;    
-        end
-        
-        default : begin
-            o_tx_start = 0;
-            o_data_tx = o_data_tx;
-            o_reg_dato_A = o_reg_dato_A;
-            o_reg_dato_B = o_reg_dato_B;
-            o_reg_opcode = o_reg_opcode;
-        end
-    
-    endcase 
+always@( * ) begin //Output logic
+   o_prueba = 0;
+   case (reg_state)
+       
+       ESPERA : begin
+           o_tx_start = 0;
+           o_data_tx_next = o_data_tx;
+           o_reg_dato_A_next = o_reg_dato_A;
+           o_reg_dato_B_next = o_reg_dato_B;
+           o_reg_opcode_next = o_reg_opcode;
+       end
+       
+       OPERANDO1 : begin
+           o_tx_start = 0;
+           o_data_tx_next = o_data_tx;
+           o_reg_dato_A_next = i_data_rx;
+           o_reg_dato_B_next = o_reg_dato_B;
+           o_reg_opcode_next = o_reg_opcode;
+       end
+       
+       OPERACION : begin
+           o_tx_start = 0;
+           o_prueba = 1;
+           o_data_tx_next = o_data_tx;
+           o_reg_dato_A_next = o_reg_dato_A;
+           o_reg_dato_B_next = o_reg_dato_B;
+           o_reg_opcode_next = i_data_rx;
+          
+       end
+       
+       OPERANDO2 : begin
+           o_tx_start = 1;
+           o_prueba = 0;
+           o_data_tx_next = i_resultado_alu; 
+           o_reg_dato_A_next = o_reg_dato_A;
+           o_reg_dato_B_next = i_data_rx;
+           o_reg_opcode_next = o_reg_opcode;    
+       end
+       
+       default : begin
+           o_tx_start = 0;
+           o_data_tx_next = o_data_tx;
+           o_reg_dato_A_next = o_reg_dato_A;
+           o_reg_dato_B_next = o_reg_dato_B;
+           o_reg_opcode_next = o_reg_opcode;
+       end
+   
+   endcase 
 end
 
 endmodule
--- TP2/Src/Sources/top_arquitectura.v
+++ TP2/Src/Sources/top_arquitectura.v
@@ -22,8 +22,8 @@ module top_arquitectura(
   i_clock, 
   i_reset,
   uart_txd_in,
-  uart_rxd_out
-  //o_leds 
+  uart_rxd_out,
+  o_leds 
   );
 
 // Parametros
@@ -40,7 +40,7 @@ input i_clock;                                  // Clock.
 input i_reset;                                  // Reset.
 input uart_txd_in;                              // Transmisor de PC.
 output uart_rxd_out;                            // Receptor de PC.
-//output [BUS_SALIDA_ALU - 1 : 0] o_leds;             // Leds.
+output [4 - 1 : 0] o_leds;             // Leds.
 
 
 
@@ -56,7 +56,8 @@ wire wire_rx_done;
 wire wire_tx_start;
 wire wire_rate_baud_generator;
 
-
+wire prueba;
+assign o_leds = prueba;
 
 // Modulo interface_circuit.
 
@@ -79,7 +80,8 @@ interface_circuit
    .o_data_tx (wire_data_tx),
    .o_reg_dato_A (wire_operando_1),
    .o_reg_dato_B (wire_operando_2),
-   .o_reg_opcode (wire_opcode)
+   .o_reg_opcode (wire_opcode),
+   .o_prueba (prueba)
    );
    
 
