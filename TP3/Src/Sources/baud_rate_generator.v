0ac1efc285d511290a8bcd44567e4bd33fcc6fe6
CondorNegro <lopez.gaston.1996@gmail.com>
2018-10-11 22:22:51 -0300

    Comments baud rate

--- TP3/Src/Sources/baud_rate_generator.v
+++ TP3/Src/Sources/baud_rate_generator.v
@@ -1,7 +1,7 @@
  `timescale 1ns / 1ps
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
-// Trabajo Practico Nro. 2. UART.
+// Trabajo Practico Nro. 3. BIP I.
 // Baud Rate Generator.
 // Integrantes: Kleiner Matias, Lopez Gaston.
 // Materia: Arquitectura de Computadoras.
