79a9bf1418952beea571feab3fc21364c578c66a
matias <kleiner.matias@gmail.com>
2018-10-16 19:51:03 -0300

    fix interface

--- TP3/Src/Sources/address_calculator.v
+++ TP3/Src/Sources/address_calculator.v
@@ -23,18 +23,18 @@ module address_calculator
 );
 
   reg [PC_CANT_BITS-1:0] reg_PC;
-  reg reg_aux_wrPC;
 
 
 
-  always @(posedge i_clock) begin
+
+  always @(negedge i_clock) begin
     if (~i_reset) begin
       reg_PC <= 0;
-      reg_aux_wrPC <= 0;
+
     end
     else begin
-      reg_aux_wrPC <= i_wrPC;
-      if (i_wrPC && ~reg_aux_wrPC) begin  // Deteccion por flanco
+
+      if (i_wrPC) begin  // Deteccion por flanco
         reg_PC <= reg_PC + SUM_DIR;
       end
       else begin
