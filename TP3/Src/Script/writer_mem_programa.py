c9fa99b48f8e10d7f1a6a85daa9c41b3705df1c5
CondorNegro <lopez.gaston.1996@gmail.com>
2018-10-15 22:19:40 -0300

    Python traductor de assembler a binario OK

--- TP3/Src/Script/writer_mem_programa.py
+++ TP3/Src/Script/writer_mem_programa.py
@@ -15,43 +15,108 @@ def FileHandler(cadenaGlobal, nombreDeArchivo):
 			print 'Error en el manejo del archivo.'
 			print 'Fin.'
 			exit(1)
-		
-def FraccToBin(Fracc):
-		for ptr2 in range(NBT):
-			binarioStr+=str(bit[ptr2])
-		return binarioStr
 
 
-
-#Escritura de archivo.
-
-cadena = ""
-contadorCoef=0
-
-TXfilterPF[::-1]
-
-for indice3 in range(len(TXfilterPF)):
-	contadorCoef+=1
-	binario= FraccToBin(TXfilterPF[indice3])
-	cadena+=binario + '\n'
-
-for indice in range(72-contadorCoef): #Agregado de ceros al final para completar las 72 filas para poder armar la tabla.
-	cadena+="00000000" + '\n'
-nombreDeArchivo='.\coeficientesBinarios.txt'
-
-FileHandler(cadena, nombreDeArchivo)
-
-cadena=""
-for indice4 in range(len(TXfilterPF)):
-	cadena+=str(TXfilterPF[indice4]) + '\n'
-
-nombreDeArchivo='.\CoeficientesFiltro.txt'
-
-FileHandler(cadena,  nombreDeArchivo)
+# Funcion de traduccion del nombre de la operacion a su opcode correspondiente.
+def getOPCODE (x):
+    return {
+        'STO': '00001',
+		'LD': '00010',
+        'LDI': '00011',
+		'ADD': '00100',
+		'ADDI': '00101',
+		'SUB': '00110',
+		'SUBI': '00111',
+		'HLT': '00000',
+    }.get (x, '11111')  #11111 es el por defecto
 
 
+#Constantes 
+WIDTH_MEM = 16
+CANT_BITS_OPERANDO = 11
+DEPTH_MEM = 1024
 
-print 'Se escribio el archivo con %d coeficientes.' %contadorCoef
 
+#Lectura de archivo.
+cadena_linea = ""
+nombreDeArchivo =  'assembler_BIP_I.txt'
+
+
+try:
+	file = open (nombreDeArchivo, 'r')
+	cadena_linea = file.read()
+	file.close()
+except:
+	print 'Error en el manejo del archivo.'
+	print 'Fin.'
+	exit(1)
+
+print '\nContenido del archivo: '
+print cadena_linea
+
+
+#Parseo del archivo.
+print '\nParseo del archivo: '
+arreglo_parseo = cadena_linea.split ('\n')
+print arreglo_parseo
+
+#Asignacion de constantes
+Patron = "#"
+constantes_letras = []
+constantes_numeros = []
+for slot in arreglo_parseo:
+	if (slot != ""):
+		if (Patron in slot):
+			constantes_letras.append (slot [1])
+			constantes_numeros.append (slot.split (" ")[1])
+
+print "\nConstantes: "
+print constantes_letras
+print constantes_numeros
+
+
+#Traduccion de instrucciones a binario.
+Patron_comment = "/"   #Es un comentario
+arreglo_binario = []
+cadena_binaria = ""
+instruccion = ""
+argumento = ""
+for comando in arreglo_parseo:
+	if ((comando != "") and (comando != " ") and (Patron_comment not in comando) and (Patron not in comando)):
+		comando_parsed = comando.split (" ")
+		instruccion = comando_parsed [0]
+		cadena_binaria = getOPCODE (instruccion)
+		if (instruccion != 'HLT'):
+			argumento = comando_parsed [1] 
+			if (argumento in constantes_letras):	#Reemplazo las constantes
+				argumento = constantes_numeros [ constantes_letras.index (argumento)]
+			number_bin = bin(int(argumento))[2:]
+			#print len(number_bin)
+			for i in range(0, CANT_BITS_OPERANDO - len(number_bin)): #Me borra los ceros a la izq
+				number_bin = '0' + number_bin
+			cadena_binaria = cadena_binaria + number_bin
+		else: #Instruccion HALT
+			cadena_binaria = cadena_binaria + '0' * CANT_BITS_OPERANDO
+		arreglo_binario.append (cadena_binaria)
+		#print cadena_binaria
+
+print "\nArreglo binario: "
+print arreglo_binario
+print "\n"
+
+#Creacion de binario a guardar en mem.
+cadena_global = ""
+for i in range (len (arreglo_binario)):
+	cadena_global = cadena_global + arreglo_binario [i] + "\n"
+for i in range (DEPTH_MEM - len (arreglo_binario)):
+	if (i < (DEPTH_MEM - 1 - len (arreglo_binario))):
+		cadena_global = cadena_global + '0' * WIDTH_MEM + "\n"
+	else:
+		cadena_global = cadena_global + '0' * WIDTH_MEM
+
+FileHandler (cadena_global, "init_ram_file.txt")
+
+print "Escritura de archivo correcta."
+print "Se escribieron %d lineas con instrucciones.\n" % len(arreglo_binario)
 
 print 'Fin'
