3a7e7da7c34391028726be67b7f5e901c585d956
matias <kleiner.matias@gmail.com>
2019-02-13 13:52:50 -0300

    mem programa revisado, test bench de mem programa revisado

--- TPFinal/Src/Sources/memoria_programa.v
+++ TPFinal/Src/Sources/memoria_programa.v
@@ -10,59 +10,74 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-//  Xilinx Single Port No Change RAM
-//  This code implements a parameterizable single-port no-change memory where when data is written
-//  to the memory, the output remains unchanged.  This is the most power efficient write mode.
-//  If a reset or enable is not necessary, it may be tied off or removed from the code.
 
+  //  Xilinx Single Port No Change RAM
+  //  This code implements a parameterizable single-port no-change memory where when data is written
+  //  to the memory, the output remains unchanged.  This is the most power efficient write mode.
+  //  If a reset or enable is not necessary, it may be tied off or removed from the code.
 module memoria_programa
-#(
-  parameter RAM_WIDTH = 16,                       // Specify RAM data width
-  parameter RAM_DEPTH = 2048,                     // Specify RAM depth (number of entries)
-  parameter RAM_PERFORMANCE = "HIGH_PERFORMANCE", // Select "HIGH_PERFORMANCE" or "LOW_LATENCY"
-  parameter INIT_FILE = "",                        // Specify name/location of RAM initialization file if using one (leave blank if not)
-  parameter CANT_BIT_RAM_DEPTH = clogb2(RAM_DEPTH)
-)
-(
-  input [clogb2(RAM_DEPTH) - 1 : 0] i_addr,   // Address bus, width determined from RAM_DEPTH
-  input [RAM_WIDTH-1:0] dina,           // RAM input data (0)
-  input i_clk,                            // Clock
-  input wea,                            // Write enable (0)
-  input ena,                            // RAM Enable, for additional power savings, disable port when not in use (1)
-  input rsta,                           // Output reset (does not affect memory contents) (0)
-  input regcea,                         // Output register enable (1)
-  input soft_reset,
-  output [RAM_WIDTH - 1 : 0] o_data,       // RAM output data
-  output reg o_reset_ack
-);
-
+    (
+    i_addr,  // Address bus, width determined from RAM_DEPTH
+    i_data,           // RAM input data
+    i_clk,                            // Clock
+    wea,                              // Write enable
+    ena,                            // RAM Enable, for additional power savings, disable port when not in use (1)
+    rsta,                           // Output reset (does not affect memory contents) (0)
+    regcea,                         // Output register enable (0)
+    soft_reset,
+    o_data,           // RAM output data
+    o_reset_ack
+    );
+  
+  
+  parameter RAM_WIDTH = 32;                  // Specify RAM data width
+  parameter RAM_DEPTH = 2048;                  // Specify RAM depth (number of entries)
+  parameter RAM_PERFORMANCE = "HIGH_PERFORMANCE"; // Select "HIGH_PERFORMANCE" or "LOW_LATENCY"
+  parameter INIT_FILE = "";                       // Specify name/location of RAM initialization file if using one (leave blank if not)  
+  
+  localparam CANT_BIT_RAM_DEPTH = clogb2(RAM_DEPTH);  
+  
+  
+  input [CANT_BIT_RAM_DEPTH-1:0] i_addr;  // Address bus, width determined from RAM_DEPTH
+  input [RAM_WIDTH-1:0] i_data;           // RAM input data
+  input i_clk;                            // Clock
+  input wea;                              // Write enable
+  input ena;                            // RAM Enable, for additional power savings, disable port when not in use (1)
+  input rsta;                           // Output reset (does not affect memory contents) (0)
+  input regcea;                         // Output register enable (0)
+  input soft_reset;
+  output [RAM_WIDTH-1:0] o_data;           // RAM output data
+  output reg o_reset_ack;
+  
+  
   reg [RAM_WIDTH - 1 : 0] BRAM [RAM_DEPTH - 1 : 0];
   reg [RAM_WIDTH - 1 : 0] ram_data = {RAM_WIDTH {1'b0}};
   reg [clogb2(RAM_DEPTH-1)-1 : 0] reg_contador;
-
+  
+  
   // The following code either initializes the memory values to a specified file or to all zeros to match hardware
   generate
     if (INIT_FILE != "") begin: use_init_file
       initial
-        $readmemb(INIT_FILE, BRAM, 0, RAM_DEPTH - 1);
+         $readmemh(INIT_FILE, BRAM, 0, RAM_DEPTH - 1);
     end else begin: init_bram_to_zero
       integer ram_index;
       initial
         for (ram_index = 0; ram_index < RAM_DEPTH; ram_index = ram_index + 1)
-          BRAM[ram_index] = {RAM_WIDTH {1'b0}};
+           BRAM[ram_index] = {RAM_WIDTH{1'b0}};
     end
   endgenerate
 
   always @(posedge i_clk) begin
     if (~soft_reset) begin
-      BRAM [reg_contador] <= 0;
+      BRAM [reg_contador] <= {RAM_WIDTH{1'b0}};
 
-      if (reg_contador == (CANT_BIT_RAM_DEPTH-1)) begin
-        reg_contador <= reg_contador + 1;
+      if (reg_contador == (RAM_DEPTH-1)) begin
+        reg_contador <= reg_contador;
         o_reset_ack <= 0;
       end
       else begin
-        reg_contador <= reg_contador;
+        reg_contador <= reg_contador + 1;
         o_reset_ack <= 1;
       end
     end
@@ -71,12 +86,11 @@ module memoria_programa
       o_reset_ack <= 1;
       if (ena)
         if (wea)
-          BRAM [i_addr] <= dina;
+          BRAM [i_addr] <= i_data;
         else
           ram_data <= BRAM [i_addr];
     end
   end
-
   //  The following code generates HIGH_PERFORMANCE (use output register) or LOW_LATENCY (no output register)
   generate
     if (RAM_PERFORMANCE == "LOW_LATENCY") begin: no_output_register
@@ -88,7 +102,7 @@ module memoria_programa
 
       // The following is a 2 clock cycle read latency with improve clock-to-out timing
 
-      reg [RAM_WIDTH - 1 : 0] reg_data_out = {RAM_WIDTH {1'b0}};
+      reg [RAM_WIDTH-1:0] reg_data_out = {RAM_WIDTH{1'b0}};
 
       always @(posedge i_clk)
         if (rsta)
@@ -104,29 +118,7 @@ module memoria_programa
   //  The following function calculates the address width based on specified RAM depth
   function integer clogb2;
     input integer depth;
-      for (clogb2 = 0; depth > 0; clogb2 = clogb2+1)
+      for (clogb2=0; depth>0; clogb2=clogb2+1)
         depth = depth >> 1;
   endfunction
-
 endmodule
-
-// The following is an instantiation template for xilinx_single_port_ram_no_change
-/*
-  //  Xilinx Single Port No Change RAM
-  xilinx_single_port_ram_no_change #(
-    .RAM_WIDTH(18),                       // Specify RAM data width
-    .RAM_DEPTH(1024),                     // Specify RAM depth (number of entries)
-    .RAM_PERFORMANCE("HIGH_PERFORMANCE"), // Select "HIGH_PERFORMANCE" or "LOW_LATENCY"
-    .INIT_FILE("")                        // Specify name/location of RAM initialization file if using one (leave blank if not)
-  ) your_instance_name (
-    .addra(addra),    // Address bus, width determined from RAM_DEPTH
-    .dina(dina),      // RAM input data, width determined from RAM_WIDTH
-    .clka(clka),      // Clock
-    .wea(wea),        // Write enable
-    .ena(ena),        // RAM Enable, for additional power savings, disable port when not in use
-    .rsta(rsta),      // Output reset (does not affect memory contents)
-    .regcea(regcea),  // Output register enable
-    .douta(douta)     // RAM output data, width determined from RAM_WIDTH
-  );
-
-*/
--- TPFinal/Src/Test_Benches/test_bench_mem_programa.v
+++ TPFinal/Src/Test_Benches/test_bench_mem_programa.v
@@ -4,7 +4,7 @@
 
 //////////////////////////////////////////////////////////////////////////////////
 // Trabajo Practico Nro. 3. BIP I.
-// Test bench del modulo memoria_programa.
+// Test bench del modulo mem_data.
 // Integrantes: Kleiner Matias, Lopez Gaston.
 // Materia: Arquitectura de Computadoras.
 // FCEFyN. UNC.
@@ -12,49 +12,70 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module test_bench_mem_programa();
+module test_bench_memoria_programa();
        
    // Parametros
-   parameter RAM_WIDTH = 16;                       // Specify RAM data width
-   parameter RAM_DEPTH = 2048;                     // Specify RAM depth (number of entries)
-   parameter RAM_PERFORMANCE = "HIGH_PERFORMANCE";      // Select "HIGH_PERFORMANCE" or "LOW_LATENCY"
-   parameter INIT_FILE = "";                       // Specify name/location of RAM initialization file if using one (leave blank if not)
-   parameter CANT_BIT_RAM_DEPTH = 11;
+   parameter RAM_WIDTH = 32;                       // Specify RAM data width
+   parameter RAM_DEPTH = 1024;                     // Specify RAM depth (number of entries)
+   parameter RAM_PERFORMANCE = "LOW_LATENCY";      // Select "HIGH_PERFORMANCE" or "LOW_LATENCY"
+   parameter INIT_FILE = "";                        // Specify name/location of RAM initialization file if using one (leave blank if not)
+   
    
    //Todo puerto de salida del modulo es un cable.
    //Todo puerto de estimulo o generacion de entrada es un registro.
    
    // Entradas.
    reg clock;                                  // Clock.
-   reg [11 - 1 : 0] reg_i_addr;
-   reg [RAM_WIDTH - 1 : 0] reg_dina;
-   reg reg_softreset;  
-   //reg [RAM_WIDTH-1:0] data_in;
-   //reg reg_wea;
-   //reg reg_regcea;
-   wire [RAM_WIDTH - 1 : 0] wire_o_data;
-   wire wire_ack;
+   reg [11-1:0] reg_i_addr;
+   reg [RAM_WIDTH-1:0] data_in;                                 
+   reg reg_wea;
+   reg reg_ena;
+   reg reg_rsta;
+   reg reg_regcea;
+   reg reg_soft_reset;
+   wire wire_o_reset_ack; 
+   wire [RAM_WIDTH-1:0] wire_o_data;
    
    
    
    initial    begin
        
        clock = 1'b0;
-       reg_i_addr = 0;
-       reg_dina = 0;
-       reg_softreset = 1;
-       
-       #10000 reg_softreset = 1'b0;
-       #10000 reg_softreset = 1'b1;
-       
-       #100000 reg_softreset = 1'b0;
-       #100000 reg_softreset = 1'b1;
-       
+       reg_wea = 1'b0;
+       reg_regcea = 1'b1;
+       data_in = 8'b11011011;
+       reg_i_addr = 11'b0000000;
+       reg_ena = 1'b1;
+       reg_rsta = 1'b0;
+       reg_soft_reset = 1'b1;
         
-       #102 reg_i_addr = 11'b0000001; // Seteo de direccion de mem.
-       #100 reg_i_addr = 11'b0000010; 
-       #100 reg_i_addr = 11'b0000011; 
-       #5000 $finish;
+       //#100 reg_regcea = 1'b1; // Lectura de posicion 0, tiene que haber un 0 porque no se guardo nada todavia.
+       
+       //#100 data_in = 8'b00001111; // Dato a guardar.
+       #20 reg_wea = 1'b1; // Ahora se guarda el dato.
+       
+       #20 reg_wea = 1'b0; 
+       
+       #20 data_in = 8'b00000101; // Dato a guardar.
+       #20 reg_i_addr = 11'b0000001; // Seteo de direccion de mem.
+       #20 reg_wea = 1'b1; // Ahora se guarda el dato.
+       #20 reg_wea = 1'b0; 
+       
+       
+       
+       #20 data_in = 8'b00000010; // Dato a guardar.
+       #20 reg_i_addr = 11'b0000010; // Seteo de direccion de mem.
+       #20 reg_wea = 1'b0; // Ahora se guarda el dato.
+       
+       
+       #20 reg_i_addr = 11'b0000011; // Lectura del 1 que se guardo.
+       
+       #100 reg_soft_reset = 1'b0;
+       #30000 reg_soft_reset = 1'b1;
+       
+       
+       
+       #500000 $finish;
    end
    
    always #2.5 clock=~clock;  // Simulacion de clock.
@@ -65,23 +86,23 @@ module test_bench_mem_programa();
 memoria_programa
    #(
         .RAM_WIDTH (RAM_WIDTH),
-        .RAM_DEPTH (RAM_PERFORMANCE),
         .RAM_PERFORMANCE (RAM_PERFORMANCE),
         .INIT_FILE (INIT_FILE),
-        .CANT_BIT_RAM_DEPTH (CANT_BIT_RAM_DEPTH)
+        .RAM_DEPTH (RAM_DEPTH)
+        
     ) 
    u_memoria_programa_1    
    (
      .i_clk (clock),
      .i_addr (reg_i_addr),
-     .dina (reg_dina),
-     .wea (0),
-     .ena (1),
-     .rsta (0),
-     .regcea (1),
-     .soft_reset (reg_softreset),
-     .o_data (wire_o_data),
-     .o_reset_ack (wire_ack)
+     .i_data (data_in),
+     .wea (reg_wea),
+     .ena (reg_ena),
+     .rsta (reg_rsta),
+     .regcea (reg_regcea),
+     .soft_reset (reg_soft_reset),
+     .o_reset_ack (wire_o_reset_ack),
+     .o_data (wire_o_data)
    );
   
 endmodule
