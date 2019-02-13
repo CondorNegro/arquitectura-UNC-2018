1b74370ac89a6863f3509cf18f2fbb31e3750b95
CondorNegro <lopez.gaston.1996@gmail.com>
2019-02-13 16:28:45 -0300

    Instrucciones tipo R en ensamblador OK

--- TPFinal/Src/Script/ensamblador.py
+++ TPFinal/Src/Script/ensamblador.py
@@ -19,23 +19,104 @@ def FileHandler(cadenaGlobal, nombreDeArchivo):
 
 
 # Funcion de traduccion del nombre de la operacion a su opcode correspondiente.
-def getOPCODE (x):
+def getOPCODE (clasificacion):
     return {
-        'SLL': '00001',
-		'LD': '00010',
-        'LDI': '00011',
-		'ADD': '00100',
-		'ADDI': '00101',
-		'SUB': '00110',
-		'SUBI': '00111',
-		'HLT': '00000',
-    }.get (x, '11111')  #11111 es el por defecto
+		'I1-0': '100000',
+		'I1-1': '100001',
+		'I1-3': '100011',
+		'I1-7': '100111',
+		'I1-4': '100100',
+		'I1-5': '100101',
+		'I1-8': '101000',
+		'I1-9': '101001',
+		'I1-11': '101011',
+		'I0-8': '001000',
+		'I0-12': '001100',
+		'I0-13': '001101',
+		'I0-14': '001110',
+		'I0-15': '001111',
+		'I0-10': '001010',
+		'I0-4': '000100',
+		'I0-5': '000101',
+		'I0-2': '000010',
+		'I0-3': '000011',
+    }.get (clasificacion, '000000')  #000000 es el por defecto
+
+def getClasificacion (instr):
+	return {
+        'SLL': 'R00',
+		'SRL': 'R00',
+		'SRA': 'R00',
+		'SLLV': 'R01',
+		'SRLV': 'R01',
+		'SRAV': 'R01',
+		'ADDU': 'R10',
+		'SUBU': 'R10',
+		'AND': 'R10',
+		'OR': 'R10',
+		'XOR': 'R10',
+		'NOR': 'R10',
+		'SLT': 'R10',
+		'JR': 'J',
+		'JALR': 'J',
+		'LB': 'I1-0',
+		'LH': 'I1-1',
+		'LW': 'I1-3',
+		'LWU': 'I1-7',
+		'LBU': 'I1-4',
+		'LHU': 'I1-5',
+		'SB': 'I1-8',
+		'SH': 'I1-9',
+		'SW': 'I1-11',
+		'ADDI': 'I0-8',
+		'ANDI': 'I0-12',
+		'ORI': 'I0-13',
+		'XORI': 'I0-14',
+		'LUI': 'I0-15',
+		'SLTI': 'I0-10',
+		'BEQ': 'I0-4',
+		'BNE': 'I0-5',
+		'J': 'I0-2',
+		'JAL': 'I0-3',
+    }.get (instr, 'X')  #000000 es el por defecto
+
+def getNumeroRegistro(R):
+	registro = bin(int(R[1:]))[2:]
+	registro = registro [len(registro) - 6 : len(registro)]
+	for i in range(0, CANT_BITS_OPERANDO - len(registro)): #Me borra los ceros a la izq
+		registro = '0' + registro
+	return registro
+
+def getLSBR (instr):
+	return {
+        'SLL': '000000',
+		'SRL': '000010',
+		'SRA': '000011',
+		'SLLV': '000100',
+		'SRLV': '000110',
+		'SRAV': '000111',
+		'ADDU': '100001',
+		'SUBU': '100011',
+		'AND': '100100',
+		'OR': '100101',
+		'XOR': '100110',
+		'NOR': '100111',
+		'SLT': '101010',
+    }.get (instr, '000000')  #000000 es el por defecto
+
+
+
+
+
+
+
 
 
 #Constantes 
 WIDTH_MEM = 32
-CANT_BITS_OPERANDO = 11
-CANT_BITS_SIN_OPCODE = 27
+CANT_BITS_OPERANDO = 5
+CANT_BITS_CEROS_R_TYPE = 5
+CANT_BITS_SIN_OPCODE = 26 #32 - OPCODE 
 DEPTH_MEM = 2048
 
 
@@ -87,20 +168,47 @@ for comando in arreglo_parseo:
 	if ((comando != "") and (comando != " ") and (Patron_comment not in comando) and (Patron not in comando)):
 		comando_parsed = comando.split (" ")
 		instruccion = comando_parsed [0]
-		cadena_binaria = getOPCODE (instruccion)
 		if (instruccion != 'HLT'):
+			clasificacion_instruccion = getClasificacion (instruccion)
+			if (clasificacion_instruccion == 'X'):
+				print 'Instruccion invalida. Fin.'
+				exit(1)
+			cadena_binaria = getOPCODE (clasificacion_instruccion)
+			if (len (cadena_binaria) != 6):
+				print 'OPCODE distinto de 6. Fin.\n'
+				print cadena_binaria
+				print len(cadena_binaria)
+				exit (1)
+			
 			argumento = comando_parsed [1]
 			argumento = argumento.split(",")
-			 
-		 	#if (argumento in constantes_letras):	#Reemplazo las constantes
-			#	argumento = constantes_numeros [ constantes_letras.index (argumento)]
-			#number_bin = bin(int(argumento))[2:]
-			#print len(number_bin)
-			#for i in range(0, CANT_BITS_OPERANDO - len(number_bin)): #Me borra los ceros a la izq
-			#	number_bin = '0' + number_bin
-			#cadena_binaria = cadena_binaria + number_bin
+			
+			if (clasificacion_instruccion == 'R00'):
+				if (argumento[2] in constantes_letras):	#Reemplazo las constantes
+					argumento[2] = constantes_numeros [ constantes_letras.index (argumento[2])]
+				number_bin = bin(int(argumento[2]))[2:]
+				for i in range(0, CANT_BITS_OPERANDO - len(number_bin)): #Me agrega los ceros a la izq
+					number_bin = '0' + number_bin
+				#print len(number_bin)
+				cadena_binaria = cadena_binaria + '0' * CANT_BITS_CEROS_R_TYPE + getNumeroRegistro (argumento[1]) +\
+				getNumeroRegistro (argumento[0]) + number_bin + getLSBR (instruccion)
+			
+			elif (clasificacion_instruccion == 'R01'):
+				#print len(number_bin)
+			    #print getNumeroRegistro (argumento[2])
+				cadena_binaria = cadena_binaria +  getNumeroRegistro (argumento[2]) +\
+				getNumeroRegistro (argumento[1]) +   getNumeroRegistro (argumento[0]) + '0' * CANT_BITS_CEROS_R_TYPE +\
+				getLSBR (instruccion)
+			
+			elif (clasificacion_instruccion == 'R10'):
+				#print len(number_bin)
+				#print getNumeroRegistro (argumento[2])
+				cadena_binaria = cadena_binaria +  getNumeroRegistro (argumento[1]) +\
+				getNumeroRegistro (argumento[2]) +   getNumeroRegistro (argumento[0]) + '0' * CANT_BITS_CEROS_R_TYPE +\
+				getLSBR (instruccion)
+				
 		else: #Instruccion HALT
-			cadena_binaria = cadena_binaria + '0' * CANT_BITS_SIN_OPCODE
+			cadena_binaria = '0' * WIDTH_MEM
 		arreglo_binario.append (cadena_binaria)
 		#print cadena_binaria
 
