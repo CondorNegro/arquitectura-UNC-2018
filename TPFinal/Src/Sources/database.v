420009cde5ff74721744c3ba2ab28f879e656d44
glopez <gopezlaston@gmail.com>
2019-03-27 18:00:05 -0300

    Database falta testear

--- TPFinal/Src/Sources/database.v
+++ TPFinal/Src/Sources/database.v
@@ -12,32 +12,94 @@
 
 module database
    #(
-       parameter ADDR_LENGTH = 11,
-       parameter LONGITUD_INSTRUCCION = 32,
-       parameter CANT_BITS_CONTROL = 3
+        parameter ADDR_LENGTH = 11,
+        parameter LONGITUD_INSTRUCCION = 32,
+        parameter CANT_BITS_CONTROL = 4,
+        parameter CANT_BITS_REGISTROS = 32,
+        parameter CANT_BITS_ALU_OP = 2,
+        parameter CANT_BITS_ALU_CONTROL = 4,
+        parameter CANT_REGISTROS = 32, 
+        parameter CANT_BITS_IMMEDIATE = 16 
    )
    (
-       input i_clock,
-       input i_soft_reset,
-       input [CANT_BITS_CONTROL - 1 : 0] i_control,
+        input i_clock,
+        input i_soft_reset,
+        input [CANT_BITS_CONTROL - 1 : 0] i_control,
 
-       input [ADDR_LENGTH - 1 : 0] i_contador_ciclos, 
+        input [ADDR_LENGTH - 1 : 0] i_contador_ciclos, 
 
-       // Intruction Fetch.
-       input [ADDR_LENGTH - 1 : 0] i_pc,
-       input [ADDR_LENGTH - 1 : 0] i_adder_pc,
-       input [LONGITUD_INSTRUCCION - 1 : 0] i_instruction_fetch,
+        // Intruction Fetch.
+        input [ADDR_LENGTH - 1 : 0] i_pc,
+        input [ADDR_LENGTH - 1 : 0] i_adder_pc,
+        input [LONGITUD_INSTRUCCION - 1 : 0] i_instruction_fetch,
 
 
-       output reg [LONGITUD_INSTRUCCION - 1 : 0] o_dato
+        // Instruction Decode.
+
+        input [ADDR_LENGTH - 1 : 0] i_branch_dir,
+        input i_branch_control,
+
+        input [CANT_BITS_REGISTROS - 1 : 0] i_data_A,
+        input [CANT_BITS_REGISTROS - 1 : 0] i_data_B,
+
+        input [CANT_BITS_IMMEDIATE - 1 : 0] i_extension_signo_constante,
+        input [clogb2 (CANT_REGISTROS - 1) - 1 : 0] i_reg_rs,
+        input [clogb2 (CANT_REGISTROS - 1) - 1 : 0] i_reg_rt,
+        input [clogb2 (CANT_REGISTROS - 1) - 1 : 0] i_reg_rd,
+       
+        // Control de instruction decode.
+
+        input i_RegDst,
+        input i_RegWrite,
+        input i_ALUSrc,
+        input [CANT_BITS_ALU_OP - 1 : 0] i_ALUOp,
+        input i_MemRead,
+        input i_MemWrite,
+        input i_MemtoReg,
+        input i_flag_branch,
+        input [CANT_BITS_ALU_CONTROL - 1 : 0] i_ALUCtrl,
+
+        output reg [LONGITUD_INSTRUCCION - 1 : 0] o_dato
 
    );
 
     reg [ADDR_LENGTH - 1 : 0] reg_contador_ciclos;
+
+    // Instruction fetch.
     reg [ADDR_LENGTH - 1 : 0] reg_pc;
     reg [ADDR_LENGTH - 1 : 0] reg_adder_pc;
     reg [LONGITUD_INSTRUCCION - 1 : 0] reg_instruction_fetch;
 
+    //Instruction decode.
+    reg [ADDR_LENGTH - 1 : 0] reg_branch_dir;
+    reg reg_branch_control;
+
+    reg [CANT_BITS_REGISTROS - 1 : 0] reg_data_A;
+    reg [CANT_BITS_REGISTROS - 1 : 0] reg_data_B;
+
+    reg [CANT_BITS_IMMEDIATE - 1 : 0] reg_extension_signo_constante;
+    reg [clogb2 (CANT_REGISTROS - 1) - 1 : 0] reg_rs;
+    reg [clogb2 (CANT_REGISTROS - 1) - 1 : 0] reg_rt;
+    reg [clogb2 (CANT_REGISTROS - 1) - 1 : 0] reg_rd;
+
+    // Instruction decode control.
+    reg reg_RegDst;
+    reg reg_RegWrite;
+    reg reg_ALUSrc;
+    reg [CANT_BITS_ALU_OP - 1 : 0] reg_ALUOp;
+    reg reg_MemRead;
+    reg reg_MemWrite;
+    reg reg_MemtoReg;
+    reg reg_flag_branch;
+    reg [CANT_BITS_ALU_CONTROL - 1 : 0] reg_ALUCtrl; 
+
+   //  The following function calculates the address width based on specified RAM depth
+    function integer clogb2;
+        input integer depth;
+            for (clogb2=0; depth>0; clogb2=clogb2+1)
+                depth = depth >> 1;
+    endfunction
+  
   always @(posedge i_clock) begin
     if (~ i_soft_reset) begin
         reg_pc <= 0;
@@ -45,49 +107,312 @@ module database
         reg_instruction_fetch <= 0;
         reg_contador_ciclos <= 0;
         o_dato <= 0;
+        reg_branch_dir <= 0;
+        reg_branch_control <= 0;
+        reg_data_A <= 0; 
+        reg_data_B <= 0;
+        reg_extension_signo_constante <= 0;
+        reg_rs <= 0;
+        reg_rt <= 0;
+        reg_rd <= 0;
+        reg_RegDst <= 0;
+        reg_RegWrite <= 0;
+        reg_ALUSrc <= 0;
+        reg_ALUOp <= 0;
+        reg_MemRead <= 0;
+        reg_MemWrite <= 0;
+        reg_MemtoReg <= 0;
+        reg_flag_branch <= 0;
+        reg_ALUCtrl <= 0;
     end
     else begin
-        if (i_control == 0) begin
+        if (i_control == 0) begin // No se hace nada, se mantienen los valores.
             reg_pc <= reg_pc;
             reg_adder_pc <= reg_adder_pc;
             reg_instruction_fetch <= reg_instruction_fetch;
             o_dato <= o_dato;
             reg_contador_ciclos <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
         end 
-        if (i_control == 1) begin
+        if (i_control == 1) begin // Se guardan los valores de las entradas en los registros.
             reg_pc <= i_pc;
             reg_adder_pc <= i_adder_pc;
             reg_instruction_fetch <= i_instruction_fetch;
             reg_contador_ciclos <= i_contador_ciclos;
             o_dato <= 0;
+            reg_branch_dir <= i_branch_dir;
+            reg_branch_control <= i_branch_control;
+            reg_data_A <= i_data_A; 
+            reg_data_B <= i_data_B;
+            reg_extension_signo_constante <= i_extension_signo_constante;
+            reg_rs <= i_reg_rs;
+            reg_rt <= i_reg_rt;
+            reg_rd <= i_reg_rd;
+            reg_RegDst <= i_RegDst;
+            reg_RegWrite <= i_RegWrite;
+            reg_ALUSrc <= i_ALUSrc;
+            reg_ALUOp <= i_ALUOp;
+            reg_MemRead <= i_MemRead;
+            reg_MemWrite <= i_MemWrite;
+            reg_MemtoReg <= i_MemtoReg;
+            reg_flag_branch <= i_flag_branch;
+            reg_ALUCtrl <= i_ALUCtrl;
         end
-        else if (i_control == 2) begin
+        else if (i_control == 2) begin // Se devuelve el contador de programa a la salida.
             reg_pc <= reg_pc;
             reg_adder_pc <= reg_adder_pc;
             reg_instruction_fetch <= reg_instruction_fetch;
             reg_contador_ciclos <= reg_contador_ciclos;
             o_dato <= reg_pc;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
         end
-        else if (i_control == 3) begin
+        else if (i_control == 3) begin // Se devuelve el contador de ciclos a la salida.
             reg_pc <= reg_pc;
             reg_adder_pc <= reg_adder_pc;
             reg_instruction_fetch <= reg_instruction_fetch;
             reg_contador_ciclos <= reg_contador_ciclos;
             o_dato <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
         end
-        else if (i_control == 4) begin
+        else if (i_control == 4) begin //Se devuelve la salida del adder del instruction fetch en la salida de este modulo.
             reg_pc <= reg_pc;
             reg_adder_pc <= reg_adder_pc;
             reg_instruction_fetch <= reg_instruction_fetch;
             reg_contador_ciclos <= reg_contador_ciclos;
             o_dato <= reg_adder_pc;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
         end
-        else if (i_control == 5) begin
+        else if (i_control == 5) begin // Se devuelve la instruccion que pasa a la etapa de ID en la salida de este modulo. 
             reg_pc <= reg_pc;
             reg_adder_pc <= reg_adder_pc;
             reg_instruction_fetch <= reg_instruction_fetch;
             reg_contador_ciclos <= reg_contador_ciclos;
             o_dato <= reg_instruction_fetch;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
+        end
+        else if (i_control == 6) begin // Se devuelve la direccion y el control del salto en la salida de este modulo. 
+            reg_pc <= reg_pc;
+            reg_adder_pc <= reg_adder_pc;
+            reg_instruction_fetch <= reg_instruction_fetch;
+            reg_contador_ciclos <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
+            o_dato <= {reg_branch_control, reg_branch_dir};
+        end
+        else if (i_control == 7) begin // Se devuelve el contenido de reg_data_A  en la salida de este modulo. 
+            reg_pc <= reg_pc;
+            reg_adder_pc <= reg_adder_pc;
+            reg_instruction_fetch <= reg_instruction_fetch;
+            reg_contador_ciclos <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
+            o_dato <= reg_data_A;
+        end
+        else if (i_control == 8) begin // Se devuelve el contenido de reg_data_B  en la salida de este modulo. 
+            reg_pc <= reg_pc;
+            reg_adder_pc <= reg_adder_pc;
+            reg_instruction_fetch <= reg_instruction_fetch;
+            reg_contador_ciclos <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
+            o_dato <= reg_data_B;
+        end
+        else if (i_control == 9) begin // Se devuelve el contenido de reg_extension_signo_constante en la salida de este modulo. 
+            reg_pc <= reg_pc;
+            reg_adder_pc <= reg_adder_pc;
+            reg_instruction_fetch <= reg_instruction_fetch;
+            reg_contador_ciclos <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
+            o_dato <= reg_extension_signo_constante;
+        end
+        else if (i_control == 10) begin // Se devuelve el contenido de reg_rs, reg_rt y reg_rd en la salida de este modulo. 
+            reg_pc <= reg_pc;
+            reg_adder_pc <= reg_adder_pc;
+            reg_instruction_fetch <= reg_instruction_fetch;
+            reg_contador_ciclos <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
+            o_dato <= {reg_rs, reg_rt, reg_rd};
+        end
+        else if (i_control == 11) begin // Se devuelve el contenido de las señales de control en la salida de este modulo. 
+            reg_pc <= reg_pc;
+            reg_adder_pc <= reg_adder_pc;
+            reg_instruction_fetch <= reg_instruction_fetch;
+            reg_contador_ciclos <= reg_contador_ciclos;
+            reg_branch_dir <= reg_branch_dir;
+            reg_branch_control <= reg_branch_control;
+            reg_data_A <= reg_data_A; 
+            reg_data_B <= reg_data_B;
+            reg_extension_signo_constante <= reg_extension_signo_constante;
+            reg_rs <= reg_rs;
+            reg_rt <= reg_rt;
+            reg_rd <= reg_rd;
+            reg_RegDst <= reg_RegDst;
+            reg_RegWrite <= reg_RegWrite;
+            reg_ALUSrc <= reg_ALUSrc;
+            reg_ALUOp <= reg_ALUOp;
+            reg_MemRead <= reg_MemRead;
+            reg_MemWrite <= reg_MemWrite;
+            reg_MemtoReg <= reg_MemtoReg;
+            reg_flag_branch <= reg_flag_branch;
+            reg_ALUCtrl <= reg_ALUCtrl;
+            o_dato <= {reg_RegDst, reg_RegWrite, reg_ALUSrc, reg_MemRead, reg_MemWrite, reg_MemtoReg, reg_ALUOp, reg_flag_branch, reg_ALUCtrl};
         end
         else begin
             reg_pc <= 0;
@@ -95,6 +420,23 @@ module database
             reg_instruction_fetch <= 0;
             reg_contador_ciclos <= 0;
             o_dato <= 0;
+            reg_branch_dir <= 0;
+            reg_branch_control <= 0;
+            reg_data_A <= 0; 
+            reg_data_B <= 0;
+            reg_extension_signo_constante <= 0;
+            reg_rs <= 0;
+            reg_rt <= 0;
+            reg_rd <= 0;
+            reg_RegDst <= 0;
+            reg_RegWrite <= 0;
+            reg_ALUSrc <= 0;
+            reg_ALUOp <= 0;
+            reg_MemRead <= 0;
+            reg_MemWrite <= 0;
+            reg_MemtoReg <= 0;
+            reg_flag_branch <= 0;
+            reg_ALUCtrl <= 0;
         end
     end
 
--- TPFinal/Src/Test_Benches/test_bench_database.v
+++ TPFinal/Src/Test_Benches/test_bench_database.v
@@ -13,9 +13,14 @@
 
 module test_bench_database();
 
-     parameter ADDR_LENGTH = 11;
-     parameter LONGITUD_INSTRUCCION = 32;
-     parameter CANT_BITS_CONTROL = 3;
+    parameter ADDR_LENGTH = 11;
+    parameter LONGITUD_INSTRUCCION = 32;
+    parameter CANT_BITS_CONTROL = 4;
+    parameter CANT_BITS_REGISTROS = 32;
+    parameter CANT_BITS_ALU_OP = 2;
+    parameter CANT_BITS_ALU_CONTROL = 4;
+    parameter CANT_REGISTROS = 32;
+    parameter CANT_BITS_IMMEDIATE = 16; 
     //Todo puerto de salida del modulo es un cable.
 	//Todo puerto de estimulo o generacion de entrada es un registro.
 
@@ -31,25 +36,86 @@ module test_bench_database();
 
     reg [ADDR_LENGTH - 1 : 0] contador_ciclos;
 
+    // Instruction decode.
+
+    reg [ADDR_LENGTH - 1 : 0] reg_branch_dir;
+    reg reg_branch_control;
+
+    reg [CANT_BITS_REGISTROS - 1 : 0] reg_data_A;
+    reg [CANT_BITS_REGISTROS - 1 : 0] reg_data_B;
+
+    reg [CANT_BITS_IMMEDIATE - 1 : 0] reg_extension_signo_constante;
+    reg [clogb2 (CANT_REGISTROS - 1) - 1 : 0] reg_rs;
+    reg [clogb2 (CANT_REGISTROS - 1) - 1 : 0] reg_rt;
+    reg [clogb2 (CANT_REGISTROS - 1) - 1 : 0] reg_rd;
+       
+    // Control de instruction decode.
+
+    reg reg_RegDst;
+    reg reg_RegWrite;
+    reg reg_ALUSrc;
+    reg [CANT_BITS_ALU_OP - 1 : 0] reg_ALUOp;
+    reg reg_MemRead;
+    reg reg_MemWrite;
+    reg reg_MemtoReg;
+    reg reg_flag_branch;
+    reg [CANT_BITS_ALU_CONTROL - 1 : 0] reg_ALUCtrl;
+
+
     wire [LONGITUD_INSTRUCCION - 1 : 0] wire_dato;
+    
+    //  The following function calculates the address width based on specified RAM depth
+    function integer clogb2;
+        input integer depth;
+            for (clogb2=0; depth>0; clogb2=clogb2+1)
+                depth = depth >> 1;
+    endfunction
 
 	initial	begin
-	   clock = 1'b0;
-	   soft_reset = 1'b1; // Reset en 0. (Normal cerrado el boton del reset).
-	   control = 0;
-	   pc = 4;
-		 pc_plus_cuatro = 8;
-		 instruction_fetch = 2;
-     contador_ciclos = 1;
-	   
-		 #10 soft_reset = 1'b0; // Desactivo la accion del reset.
-	   #10 soft_reset = 1'b1; // Desactivo la accion del reset.
-	   
-		 #10 control = 1;
-	  
-		 #20 control = 3;
+	    clock = 1'b0;
+	    soft_reset = 1'b1; // Reset en 0. (Normal cerrado el boton del reset).
+	    control = 0;
+	    pc = 4;
+		pc_plus_cuatro = 8;
+		instruction_fetch = 2;
+        contador_ciclos = 1;
+        reg_branch_dir = 1;
+        reg_branch_control = 1;
+        reg_data_A = 2;
+        reg_data_B = 3; 
+        reg_extension_signo_constante = 4;
+        reg_rs = 5;
+        reg_rt = 6;
+        reg_rd = 7;
+        reg_RegDst = 1;
+        reg_RegWrite = 0;
+        reg_ALUSrc = 0;
+        reg_ALUOp = 2;
+        reg_MemRead = 0;
+        reg_MemWrite = 1;
+        reg_MemtoReg = 0;
+        reg_flag_branch = 1;
+        reg_ALUCtrl = 0;
 
-		 #20 control = 6;
+
+	   
+		#10 soft_reset = 1'b0; // Desactivo la accion del reset.
+	    #10 soft_reset = 1'b1; // Desactivo la accion del reset.
+	   
+		#10 control = 1;
+	  
+		#20 control = 3;
+
+		#20 control = 6;
+
+        #20 control = 7;
+
+		#20 control = 8;
+
+        #20 control = 9;
+
+		#20 control = 11;
+
 
 		// Test 4: Prueba reset.
 		#10000 soft_reset = 1'b0; // Reset.
@@ -65,20 +131,43 @@ module test_bench_database();
 // Modulo para pasarle los estimulos del banco de pruebas.
 database
     #(
-         .ADDR_LENGTH (ADDR_LENGTH),
-         .LONGITUD_INSTRUCCION (LONGITUD_INSTRUCCION),
-				 .CANT_BITS_CONTROL (CANT_BITS_CONTROL)
+        .ADDR_LENGTH (ADDR_LENGTH),
+        .LONGITUD_INSTRUCCION (LONGITUD_INSTRUCCION),
+		.CANT_BITS_CONTROL (CANT_BITS_CONTROL),
+        .CANT_BITS_REGISTROS (CANT_BITS_REGISTROS),
+        .CANT_BITS_ALU_OP (CANT_BITS_ALU_OP),
+        .CANT_BITS_ALU_CONTROL (CANT_BITS_ALU_CONTROL),
+        .CANT_REGISTROS (CANT_REGISTROS),
+        .CANT_BITS_IMMEDIATE (CANT_BITS_IMMEDIATE)
+
      )
     u_database_1
     (
-      .i_clock (clock),
-      .i_soft_reset (soft_reset),
-      .i_control (control),
-      .i_pc (pc),
-			.i_pc_plus_cuatro (pc_plus_cuatro),
-			.i_instruction_fetch (instruction_fetch),
-      .i_contador_ciclos (contador_ciclos),
-      .o_dato (wire_dato)
+        .i_clock (clock),
+        .i_soft_reset (soft_reset),
+        .i_control (control),
+        .i_pc (pc),
+		.i_pc_plus_cuatro (pc_plus_cuatro),
+		.i_instruction_fetch (instruction_fetch),
+        .i_contador_ciclos (contador_ciclos),
+        .i_branch_dir (reg_branch_dir),
+        .i_branch_control (reg_branch_control),
+        .i_data_A (reg_data_A),
+        .i_data_B (reg_data_B),
+        .i_extension_signo_constante (reg_extension_signo_constante),
+        .i_reg_rs (reg_rs),
+        .i_reg_rt (reg_rt),
+        .i_reg_rd (reg_rd),
+        .i_RegDst (reg_RegDst),
+        .i_RegWrite (reg_RegWrite),
+        .i_ALUSrc (reg_ALUSrc),
+        .i_ALUOp (reg_ALUOp),
+        .i_MemRead (reg_MemRead),
+        .i_MemWrite (reg_MemWrite),
+        .i_MemtoReg (reg_MemtoReg),
+        .i_flag_branch (reg_flag_branch),
+        .i_ALUCtrl (reg_ALUCtrl),
+        .o_dato (wire_dato)
     );
 
 endmodule
