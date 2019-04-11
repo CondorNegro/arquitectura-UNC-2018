8d065c60e8a38466922a01b236ac988e5ed7d367
glopez <gopezlaston@gmail.com>
2019-04-11 11:28:17 -0300

    Debug unit ok

--- TPFinal/Src/Test_Benches/test_bench_debug_unit.v
+++ TPFinal/Src/Test_Benches/test_bench_debug_unit.v
@@ -18,11 +18,12 @@ module test_bench_debug_unit();
   parameter HALT_INSTRUCTION = 32'hFFFFFFFF; //  Opcode de la instruccion HALT.
   parameter ADDR_MEM_PROG_LENGTH = 10;      //  Cantidad de bits del bus de direcciones de la memoria de programa.
   parameter ADDR_MEM_DATOS_LENGTH = 10;     //  Cantidad de bits del bus de direcciones de la memoria de datos.
-  parameter CANTIDAD_ESTADOS = 10;      //  Cantidad de estados
+  parameter CANTIDAD_ESTADOS = 12;      //  Cantidad de estados
   parameter LONGITUD_INSTRUCCION = 32;  //  Cantidad de bits de la instruccion
   parameter CANT_BITS_REGISTRO = 32;
-  parameter CANT_DATOS_DATABASE = 12; // Cantidad de datos a traer del database
+  parameter CANT_DATOS_DATABASE = 3; // Cantidad de datos a traer del database
   parameter CANT_COLUMNAS_MEM_DATOS = 4;
+  parameter RAM_DATOS_DEPTH = 1024;
    
    //Todo puerto de salida del modulo es un cable.
    //Todo puerto de estimulo o generacion de entrada es un registro.
@@ -70,118 +71,118 @@ module test_bench_debug_unit();
   endfunction
    
    initial    begin
-       reg_i_clock = 1'b0;
-       reg_i_reset = 1'b1; // Reset en 0, se resetea cuando se pone a 1. (Normal cerrado el boton del reset).
-       reg_i_tx_done = 1'b0;
-       reg_i_data_rx = 0;
-       reg_i_rx_done = 1'b0; //tiene que pasar a 1 cuando recibe el dato completamente.
-       reg_i_soft_reset_ack = 1'b1; //despues tiene que valer 0.
-       reg_flag_halt = 0;
-       reg_dato_database = 32'b10101010111111110000000011110000;
-       reg_dato_mem_datos = 0;
-       reg_bit_sucio = 0; 
+        reg_i_clock = 1'b0;
+        reg_i_reset = 1'b1; // Reset en 0, se resetea cuando se pone a 1. (Normal cerrado el boton del reset).
+        reg_i_tx_done = 1'b0;
+        reg_i_data_rx = 0;
+        reg_i_rx_done = 1'b0; //tiene que pasar a 1 cuando recibe el dato completamente.
+        reg_i_soft_reset_ack = 1'b1; //despues tiene que valer 0.
+        reg_flag_halt = 0;
+        reg_dato_database = 32'b10101010111111110000000011110000;
+        reg_dato_mem_datos = 10;
+        reg_bit_sucio = 0; 
 
         
-         #10 reg_i_reset = 1'b0;
-         #10 reg_i_reset = 1'b1;
+        #10 reg_i_reset = 1'b0;
+        #10 reg_i_reset = 1'b1;
         
-       //hasta aca estoy en el estado 1 (ESPERA).
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       
-       //ahora paso a estado 2 (SOFT_RESET).
-       #50 reg_i_soft_reset_ack = 1'b0;
-       
-       //ahora paso a estado 3 (ESPERA_PC_ACK).
-       #10 reg_i_data_rx = 1'b1; 
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       
-       //ahora paso a estado 4 (READ_PROGRAMA), cargo el programa.
-       #10 reg_i_data_rx = 4'b1011;
-       
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
-              
-       #10 reg_i_data_rx = 4'b1001;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
-       
-       #10 reg_i_data_rx = 4'b1111;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
-       
-       
-       #10 reg_i_data_rx = 0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0;
-       #10 reg_i_rx_done = 1'b1;
-       #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       //MANDO HALT PARA SALIR DE READ PROGRAMA
+        //hasta aca estoy en el estado 1 (ESPERA).
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        
+        //ahora paso a estado 2 (SOFT_RESET).
+        #50 reg_i_soft_reset_ack = 1'b0;
+        
+        //ahora paso a estado 3 (ESPERA_PC_ACK).
+        #10 reg_i_data_rx = 1'b1; 
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        
+        //ahora paso a estado 4 (READ_PROGRAMA), cargo el programa.
+        #10 reg_i_data_rx = 4'b1011;
+        
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
+                
+        #10 reg_i_data_rx = 4'b1001;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
+        
+        #10 reg_i_data_rx = 4'b1111;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
+        
+        
+        #10 reg_i_data_rx = 0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0; //1 instruccion, 4 veces tengo que mandar el rx done (8bits x 4 = 32)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //MANDO HALT PARA SALIR DE READ PROGRAMA
         #10 reg_i_data_rx = 8'b11111111;
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
-     
-             
+    
+            
         #10 reg_i_data_rx = 8'b11111111;
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
-      
+    
         #10 reg_i_data_rx = 8'b11111111;
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
-      
-      
+        
+        
         #10 reg_i_data_rx = 8'b11111111;
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
 
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         //ahora paso a estado 5 (ESPERA_START).
         #10 reg_i_data_rx = 8'b00000111;
         #10 reg_i_rx_done = 1'b1;
@@ -191,7 +192,7 @@ module test_bench_debug_unit();
         #10 reg_flag_halt = 0;
 
         // Transmision de datos desde placa a PC
-        
+        //Dato 1
         #10 reg_i_data_rx = 8'b00001000; // PART3
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
@@ -213,15 +214,15 @@ module test_bench_debug_unit();
         #10 reg_i_rx_done = 1'b0;
 
 
-          //ahora paso a estado de ESPERA.
-        #10 reg_flag_halt = 1;
+        
+        
 
         #10 reg_i_data_rx = 8'b10100000; //NO HAGO NADA
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
 
 
-
+        // Dato 2
         #10 reg_i_data_rx = 8'b00001000; // PART3
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
@@ -244,55 +245,7 @@ module test_bench_debug_unit();
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
 
-
-        #10 reg_i_data_rx = 8'b00001000; // PART3
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-
-
-
-        #10 reg_i_data_rx = 8'b00010000; // PART2
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-
-
-
-        #10 reg_i_data_rx = 8'b00011000; //PART1
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-
-
-
-        #10 reg_i_data_rx = 8'b00100000; //PART0
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-        
- 
-
-        #10 reg_i_data_rx = 8'b00001000; // PART3
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-
-
-
-        #10 reg_i_data_rx = 8'b00010000; // PART2
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-
-
-
-        #10 reg_i_data_rx = 8'b00011000; //PART1
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-
-
-
-        #10 reg_i_data_rx = 8'b00100000; //PART0
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-       
-       
-
+        // Dato 3
         #10 reg_i_data_rx = 8'b00001000; // PART3
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
@@ -315,136 +268,45 @@ module test_bench_debug_unit();
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
         
+        // Activo HALT
+        #10 reg_flag_halt = 1;
         
-        
-        #10 reg_i_data_rx = 8'b00001000; // PART3
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-        #10 reg_i_data_rx = 8'b00010000; // PART2
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-        #10 reg_i_data_rx = 8'b00011000; //PART1
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-        #10 reg_i_data_rx = 8'b00100000; //PART0
-        #10 reg_i_rx_done = 1'b1;
-        #10 reg_i_rx_done = 1'b0;
-            
-            
-            
-            
-          #10 reg_i_data_rx = 8'b00001000; // PART3
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-          #10 reg_i_data_rx = 8'b00010000; // PART2
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-          #10 reg_i_data_rx = 8'b00011000; //PART1
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-          #10 reg_i_data_rx = 8'b00100000; //PART0
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-                     
-                     
-                     
-                     
-          #10 reg_i_data_rx = 8'b00001000; // PART3
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-    
-    
-    
-          #10 reg_i_data_rx = 8'b00010000; // PART2
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-    
-    
-    
-          #10 reg_i_data_rx = 8'b00011000; //PART1
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-    
-    
-    
-          #10 reg_i_data_rx = 8'b00100000; //PART0
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-                            
-                            
-                            
-                            
-          #10 reg_i_data_rx = 8'b00001000; // PART3
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-          #10 reg_i_data_rx = 8'b00010000; // PART2
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-          #10 reg_i_data_rx = 8'b00011000; //PART1
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
-  
-  
-  
-          #10 reg_i_data_rx = 8'b00100000; //PART0
-          #10 reg_i_rx_done = 1'b1;
-          #10 reg_i_rx_done = 1'b0;
+
+        #5 reg_bit_sucio = 1'b1;
+        #50 reg_bit_sucio = 1'b0;
 
 
         #10 reg_i_data_rx = 8'b00001000; // PART3
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
 
-
-
+        
         #10 reg_i_data_rx = 8'b00010000; // PART2
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
 
-
-
-        #10 reg_i_data_rx = 8'b00011000; //PART1
+        
+        #10 reg_i_data_rx = 8'b00011000; // PART1
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
 
 
 
-        #10 reg_i_data_rx = 8'b00100000; //PART0
+        #10 reg_i_data_rx = 8'b00100000; // PART0
         #10 reg_i_rx_done = 1'b1;
         #10 reg_i_rx_done = 1'b0;
 
-       
-       
+        #250000 reg_i_data_rx = 8'b00101000; //Salgo de FIN MEM CHECK
+        #10 reg_i_rx_done = 1'b1;
+        #10 reg_i_rx_done = 1'b0;
+        
+        
 
-       
-       
-       // Test 5: Prueba reset.
-       #500000 reg_i_reset = 1'b1; // Reset.
-       #10 reg_i_reset = 1'b0; // Desactivo el reset.
+        
+        
+        // Test 5: Prueba reset.
+        #500000 reg_i_reset = 1'b1; // Reset.
+        #10 reg_i_reset = 1'b0; // Desactivo el reset.
 
        #1000000 $finish;
    end
@@ -464,7 +326,8 @@ debug_unit
         .LONGITUD_INSTRUCCION (LONGITUD_INSTRUCCION),
         .CANT_DATOS_DATABASE (CANT_DATOS_DATABASE),
         .CANT_COLUMNAS_MEM_DATOS (CANT_COLUMNAS_MEM_DATOS),
-        .CANT_BITS_REGISTRO (CANT_BITS_REGISTRO)
+        .CANT_BITS_REGISTRO (CANT_BITS_REGISTRO),
+        .RAM_DATOS_DEPTH (RAM_DATOS_DEPTH)
     ) 
    u_debug_unit_1    // Una sola instancia de este modulo.
    (
